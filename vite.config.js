60ca205ce90a076a8c58d300a9e192dd1094f402
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-06-24 16:17:08 +0300

    17 commit

--- vite.config.js
+++ vite.config.js
@@ -20,45 +20,6 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
-    html({
-      template: ({ attributes, files, publicPath, title }) => {
-        return `
-          <!DOCTYPE html>
-          <html ${attributes.html}>
-            <head>
-              <meta charset="UTF-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <title>${title}</title>
-              ${
-                files.css
-                  ? files.css.map((file) => {
-                      return `<link rel="stylesheet" href="${
-                        publicPath + file.fileName
-                      }">`;
-                    }).join('')
-                  : ''
-              }
-            </head>
-            <body>
-              <div></div>
-              ${
-                files.js
-                  ? files.js.map((file) => {
-                    return `<script type="module" src="${
-                      publicPath + file.fileName
-                    }" ${Object.keys(attributes.script)
-                      .map((key) => `${key}="${attributes.script[key]}"`)
-                      .join(' ')}></script>`;
-                  }).join('')
-                  : ''
-              }
-            </body>
-          </html>
-        `;
-      },
-    }),
-  ],
   // Specify assets to be copied directly to 'dist'
   assetsInclude: ['data/**'],
 });
